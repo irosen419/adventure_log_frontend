1147fdfd2404a51510fb004ff5e439dfdbb0d5cd
irosen419 <irosen419@gmail.com>
2020-12-10 21:26:57 -0500

    fixed issue where page redirected to wrong page after trip deletion.

--- src/Containers/TripShow.js
+++ src/Containers/TripShow.js
@@ -101,7 +101,7 @@ class TripShow extends React.Component {
             .then(resp => resp.json())
             .then((data) => {
                 if (data.success) {
-                    this.props.history.push(`/dashboard/${this.state.trip.user_id}`)
+                    this.props.history.push(`/dashboard/${this.props.user.id}`)
                 } else {
                     console.log(data.error)
                 }
